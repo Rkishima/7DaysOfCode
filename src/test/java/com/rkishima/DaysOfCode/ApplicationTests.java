01f99b5616f7b39f0fdfc1082ee90a69b74a68ee
Ricardo Kishima <ricardokishima@hotmail.com>
2022-04-06 19:25:33 -0300

    first commit

---
+++ src/test/java/com/rkishima/DaysOfCode/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkishima.DaysOfCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
